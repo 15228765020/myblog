b323909b96545f92ac9b8cc2d53d488a13726dfd
cl <p1690533359@163.com>
2022-08-20 02:19:06 +0800

    博客相册首次提交

---
+++ src/main/java/com/my/blog/config/MyViewConfig.java
@@ -0,0 +1,15 @@
+package com.my.blog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyViewConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //我的相册
+        registry.addViewController("/album").setViewName("photo_album.html");
+    }
+}
